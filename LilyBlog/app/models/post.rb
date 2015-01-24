51cbe556149a7d60cb28f2c475c517b1ac8b944c
patriciacarbajal <carbajal.patricia@gmail.com>
2015-01-23 20:43:55 -0500

    now able to post images to blog with amazon s3 storage

--- LilyBlog/app/models/post.rb
+++ LilyBlog/app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ActiveRecord::Base 
   extend ::FriendlyId 
   friendly_id :title, use: :slugged 
-  validates :title, uniqueness: true 
+  validates :title, presence: true 
   # validates :body, presence: true 
   validates :color, presence: true
 
-  mount_uploader :image, AvatarUploader
+  mount_uploader :image, ImageUploader
 
 end 
