734bc558f7503e4bd301313d7f9074108f04fbf0
patriciacarbajal <carbajal.patricia@gmail.com>
2015-01-25 20:40:32 -0500

    Revert "categories- display posts by category"
    This reverts commit 676cd7f5c6a1d47dca111bdeaea052bf8a4d431b.

--- LilyBlog/app/controllers/categories_controller.rb
+++ LilyBlog/app/controllers/categories_controller.rb
@@ -7,7 +7,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @posts = Post.where(posts_category_id: @category.id)
+    @post = @category.posts.find(params[:id])
   end
 
 end
