3b8d42ed591f6d74c17c9f084a68ab42c7fa5fd3
patriciacarbajal <carbajal.patricia@gmail.com>
2015-01-26 20:39:28 -0500

    pinterest style images

--- LilyBlog/app/assets/javascripts/main.js
+++ LilyBlog/app/assets/javascripts/main.js
@@ -1,11 +1,56 @@
 $(document).ready(function(){
 
+$(function(){
+  $('.posts-container').center();
+  setupPosts();
+  $(window).resize(setupPosts); 
+});
+
+
   jQuery.fn.center = function () {
     this.css("position","absolute");
     this.css("top", ( jQuery(window).height() - this.height() ) / 2+jQuery(window).scrollTop() + "px");
     this.css("left", ( jQuery(window).width() - this.width() ) / 2+jQuery(window).scrollLeft() + "px");
     return this;
   }
+
+  var colCount = 0;
+  var colWidth = 0;
+  var margin = 20;
+  var windowWidth = 0;
+  var spaceLeft = 0;
+  var posts = [];
+
+  function setupPosts() {
+      windowWidth = $(window).width();
+      colWidth = $('.post').outerWidth();
+      // colCount = Math.floor(windowWidth/(colWidth+margin));
+      colCount = Math.floor(windowWidth/(colWidth+margin*2));
+      spaceLeft = (windowWidth - ((colWidth*colCount)+(margin*(colCount-1)))) / 2;
+      for(var i=0; i < colCount; i++) {
+        posts.push(margin);
+      }
+      positionPosts();
+  }
+
+  function positionPosts() {
+    $('.post').each(function(){
+        var min = Array.min(posts);
+        var index = $.inArray(min, posts);
+        var leftPos = margin+(index*(colWidth+margin));
+        debugger
+        $(this).css({
+            'left':(leftPos+ spaceLeft)/18 +'%',
+            'top':min/7+'%'
+        });
+        posts[index] = min + $(this).outerHeight()+margin;
+    });
+}
+
+  // Function to get the Min value in Array
+  Array.min = function(array) {
+      return Math.min.apply(Math, array);
+  };
 
   $(".post").click(function(e){
     e.preventDefault();
